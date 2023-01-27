52484542e4e71838b286f533bdd3fe1f16d97154
Solaire-Kun <vinciguerraluca03@hotmail.com>
2023-01-27 21:45:56 +0100

    small update

--- js/script.js
+++ js/script.js
@@ -5,31 +5,32 @@ const plusBtn = document.getElementById("plus");
 const minusBtn = document.getElementById("minus");
 const clearBtn = document.getElementById("clear");
 
+// Initialize counter
+let count = 0
+const MAX_counterElement = 10; // Set the counterElement MAX limit to 10
+const MIN_counterElement = 0;  // Set the counterElement MIN limit to 0
+
 // Add HTML text content for counterElement
-counterElement.innerHTML = 0;
+counterElement.innerHTML = count;
 plusBtn.innerHTML = "+";
 minusBtn.innerHTML = "-";
 clearBtn.innerHTML = "Clear";
 
 // counterElement function
-let count = 0
-const MAX_counterElement = 10; // Set the counterElement MAX limit to 10
-const MIN_counterElement = 0;  // Set the counterElement MIN limit to 0
-
 plusBtn.addEventListener("click", () => {
     count = Math.min(count + 1, MAX_counterElement);
     counterElement.innerHTML = count;
     const isMax = (count === 10);
-    counter.style.color = isMax ? 'red' : 'white';
-    limitAlert.innerHTML = isMax ? `YOU CAN'T HOLD MORE THAN 10 ITEMS IN YOUR CART` : '';
+    counterElement.style.color = isMax ? 'red' : 'white';
+    limitAlert.innerHTML = isMax ? `You can't hold more than 10 items in your cart.` : '';
 });
 
 minusBtn.addEventListener("click", () => {
     count = Math.max(count - 1, MIN_counterElement);
     counterElement.innerHTML = count;
     const isMin = (count === 0);
-    counter.style.color = isMin ? 'red' : 'white';
-    limitAlert.innerHTML = isMin ? `YOU CAN'T HOLD MORE THAN 10 ITEMS IN YOUR CART` : '';
+    counterElement.style.color = isMin ? 'red' : 'white';
+    limitAlert.innerHTML = isMin ? `You can't hold less than 0 items in your cart.` : '';
 });
 
 clearBtn.addEventListener("click", () => {
